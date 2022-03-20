87633630778912782ad2b03b42da94d639866e97
Zafir Stoyanov <85336807+ZPStoyanov20@users.noreply.github.com>
2022-03-20 17:12:09 +0200

    Update JavaScript.js

--- Scripts/JavaScript.js
+++ Scripts/JavaScript.js
@@ -12,3 +12,63 @@ function Spain()
 {
     alert("Spain");
 }
+
+function Germany()
+{
+    alert("Germany");
+}
+
+function England()
+{
+    alert("England");
+}
+
+function ireland()
+{
+    alert("Ireland");
+}
+
+function sweden()
+{
+    alert("Sweden");
+}
+
+function Finland()
+{
+    alert("Finland");
+}
+
+function norway()
+{
+    alert("norway");
+}
+
+function iceland()
+{
+    alert("iceland");
+}
+
+function portugal()
+{
+    alert("portugal");
+}
+
+function Netherlands()
+{
+    alert("Netherlands");
+}
+
+function Belgium()
+{
+    alert("Belgium");
+}
+
+function Austria()
+{
+    alert("Austria");
+}
+
+function Switzerland()
+{
+    alert("Switzerland");
+}
